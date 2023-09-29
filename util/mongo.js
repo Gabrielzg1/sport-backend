9ff462d62ec534638f969ece162d0088814bf89a
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-09-29 16:50:23 -0300

    .

--- util/mongo.js
+++ util/mongo.js
@@ -2,20 +2,20 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 module.exports = () => {
-	// ----- Ao utilizar o docker trocar para este URL ----
-	const uri = "mongodb://mongo:27017/admin";
-	//const uri = "mongodb://localhost:27017/admin";
+  // ----- Ao utilizar o docker trocar para este URL ----
+  //const uri = "mongodb://mongo:27017/admin";
+  const uri = "mongodb://localhost:27017/admin";
 
-	mongoose
-		.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-		.then(() => {
-			console.log("MONGODB - Conexão com o banco de dados estabelecida");
-		})
-		.catch((error) => {
-			console.error("Erro ao conectar-se ao banco de dados:", error);
-		});
+    .then(() => {
+      console.log("MONGODB - Conexão com o banco de dados estabelecida");
+    })
+    .catch((error) => {
+      console.error("Erro ao conectar-se ao banco de dados:", error);
+    });
 };
