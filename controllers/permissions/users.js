99cfe1fb635e5ba5d670fba047e451cdeb6e38e2
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-10-01 18:11:44 -0300

    Update users.js

--- controllers/permissions/users.js
+++ controllers/permissions/users.js
@@ -1,105 +1,90 @@
-const express = require('express')
-const Usuario = require("../../models/permissions/user")
+const express = require("express");
+const Usuario = require("../../models/permissions/user");
 
-class UsuarioController{
+class UsuarioController {
+  async showall(req, res) {
+    try {
+      const auth = await Usuario.find();
+      return res.json(auth).status(200);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
 
+  async showid(req, res) {
+    try {
+      const { id } = req.params;
+      const auth = await Usuario.findById(id);
 
-    async showall(req,res){
+      if (auth != null) {
+        return res.json(auth).status(200);
+      } else {
+        return res.status(400).send("Usuario Inexistente");
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
 
-        try{
-            const auth = await Usuario.find();
-            return res.json(auth).status(200);
-        }
-        catch(err){
-            res.status(500).send(err)
-        }
+  async login(req, res) {
+    const { email, password } = req.body;
+    const auth = await Usuario.findOne({ email: req.body.email });
+
+    if (!auth) {
+      return res.status(400).send("Usuario Inexistente");
     }
 
-    async showid(req,res){
+    try {
+      if (auth.senha !== req.body.senha) {
+        res.send("Senha incorreta!");
+      } else {
+        res.send("Logado com sucesso");
+      }
+    } catch (error) {
+      res.status(500).send("Erro: " + error);
+    }
+  }
 
-        try{
-            const {id}= req.params;
-            const auth = await Usuario.findById(id);
+  async register(req, res) {
+    const authuser = await Usuario.findOne({ email: req.body.email });
 
-            if(auth!=null){
-                return res.json(auth).status(200);
-            }
-            else{
-                return res.status(400).send("Usuario Inexistente")
-            }
-        }
-        catch(err){
-            res.status(500).send(err)
-        }
+    if (authuser) {
+      return res.status(400).send("Email de Usuario ja existente");
     }
 
-    async login(req,res){
+    new Usuario({
+      nome: req.body.nome,
+      senha: req.body.senha,
+      email: req.body.email,
+    })
+      .save()
+      .then(() => {
+        return res.status(201).send("Criado com sucesso");
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.send("Erro ao criar usuario " + err);
+      });
+  }
 
+  async delete(req, res) {
+    const { id } = req.params;
+    const authuser = await Usuario.findById(id);
 
-        const { email, password } = req.body;
-        const auth = await Usuario.findOne({email: req.body.email})  
-        
-        if(!auth){
-            return res.status(400).send("Usuario Inexistente")
-        }
+    try {
+      if (authuser) {
+        await Usuario.deleteOne(authuser);
 
-        try{
-            if(auth.senha !== req.body.senha){
-                res.send("Senha incorreta!")
-            }
-            else{
-                res.send("Logado com sucesso")
-            }
-
-        }
-        catch(error){
-            res.status(500).send("Erro: "+ error)
-        }
+        return res.status(201).json({
+          msg: "FOI",
+        });
+      } else {
+        return res.status(400).send("Usuario Inexistente");
+      }
+    } catch (err) {
+      return res.status(500).send(err);
     }
-
-    async register(req,res){
-
-        const authuser = await Usuario.findOne({email: req.body.email});
-
-        if(authuser){
-            return res.status(400).send("Email de Usuario ja existente")
-        }
-
-        new Usuario({
-            nome:req.body.nome,
-            senha:req.body.senha,
-            email:req.body.email
-        }).save().then(()=>{
-           return res.status(201).send("Criado com sucesso")
-        }).catch((err)=>{
-            console.log(err)
-           return res.send("Erro ao criar usuario "+ err)
-        })
-
-    }
-
-    async delete(req,res){
-
-        const {id}= req.params;
-        const authuser = await Usuario.findById(id);
-
-        try{
-            if(authuser){
-                await Usuario.deleteOne(authuser);
-
-                return res.status(201).json({
-                    msg:"FOI"
-                })
-            }
-            else{
-                return res.status(400).send("Usuario Inexistente");
-            }
-        }
-        catch(err){
-            return res.status(500).send(err);
-        }
-
-    }
+  }
 }
 
 module.exports = new UsuarioController();
